d064dd61c6511078d829c2f73ccfea3f1a11d8d5
NechaevMichael <nechaevmichael@mail.ru>
2022-11-21 22:39:02 +0500

    correct task2

--- task2.py
+++ task2.py
@@ -1,17 +1,27 @@
 # Напишите программу, которая принимает на вход два числа и проверяет, 
 # является ли одно число квадратом другого.
 
-a = int(input())
-b = int(input())
+# a = int(input())
+# b = int(input())
 
 # if a == b ** 2 or a ** 2 == b:
 #     print(f'Число {a} квадрат {b}')
 # else:
 #     print('Не является')
 
-if a == b ** 2:
-    print('OK')
-elif b == a ** 2:
-    print('OK')
-else:
-    print('NO')
+# if a == b ** 2:
+#     print('OK')
+# elif b == a ** 2:
+#     print('OK')
+# else:
+#     print('NO')
+
+# Напишите программу, которая на вход принимает 5 числе и находит максимальное из них.
+
+a = int(input())
+max = a
+for i in range(4):
+    b = int(input())
+    if b > max:
+        max = b
+print(max)
