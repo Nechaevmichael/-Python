466d22d70ad954244da3aa734c1489bf4660eee5
NechaevMichael <nechaevmichael@mail.ru>
2022-11-26 22:30:46 +0500

    correct task17

--- task17.py
+++ task17.py
@@ -2,14 +2,20 @@
 # Позиции хранятся в файле file.txt в одной строке одно число.(для продвинутых - с файлом, вариант минимум - ввести позиции в консоли) -2 -1 0 1 2 Позиции: 0,1 -> 2
 
 
-n = int(input())
-a = []
-for i in range(-n, n + 1):
-    a.append(i)
+# n = int(input())
+# a = []
+# for i in range(-n, n + 1):
+#     a.append(i)
 
-position = list(map(int, input().split(',')))
+# position = list(map(int, input().split(',')))
 
-result = 1
-for i in range(len(position)):
-    result *= a[position[i]]
-print(result)    
+# result = 1
+# for i in range(len(position)):
+#     result *= a[position[i]]
+# print(result)
+
+
+digits = 'Много цифр'
+data = open('file.txt', 'w', encoding='utf-8')
+data.writelines(digits)
+data.close()
