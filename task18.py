86b59f42ac5630a8b5d74569d832a587d6889967
NechaevMichael <nechaevmichael@mail.ru>
2022-11-27 16:32:19 +0500

    add task18

--- task18.py
+++ task18.py
@@ -5,5 +5,7 @@ print(n)
 import random
 
 for i in range(len(n)):
-    n[i] = n[random.randint(- 1, len(n) - 1)]
+    # n[i] = n[random.randint(- 1, len(n) - 1)]
+    rand_pos = random.randint(- 1, len(n) - 1)
+    n[i],n[rand_pos] = n[rand_pos],n[i]
 print(n)
