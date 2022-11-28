828efa0706b5d77eb0199a0aa67f6937ffaf5d05
NechaevMichael <nechaevmichael@mail.ru>
2022-11-28 23:12:50 +0500

    correct task18

--- task18.py
+++ task18.py
@@ -6,6 +6,6 @@ print(n)
 
 for i in range(len(n)):
     # n[i] = n[random.randint(- 1, len(n) - 1)]
-    rand_pos = random.randint(- 1, len(n) - 1)
+    rand_pos = random.randint(0, len(n) - 1)
     n[i], n[rand_pos] = n[rand_pos], n[i]
 print(n)
