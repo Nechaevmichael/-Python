f40a6057192e5fb629f9e0c7924a624bf5692865
NechaevMichael <nechaevmichael@mail.ru>
2022-12-08 23:50:01 +0500

    correct task38

--- task38.py
+++ task38.py
@@ -8,12 +8,15 @@
 # Вывод:
 # (('house', 'дом'), ('car', 'машина'), ('men', 'человек'), ('tree', 'дерево'))
 
-line = 'house=дом car=машина men=человек tree=дерево'
-result = []
-for l in line.split():
-  s = tuple(l.split('='))
-  result.append(s)
-print(tuple(result))
+line = 'house=дом car=машина men=человек tree=дерево'.split()
+# result = []
+# for l in line.split():
+#   s = tuple(l.split('='))
+#   result.append(s)
+# print(tuple(result))
 
-tp = tuple(tuple(l.split('=')) for l in line.split())
-print(tp)
+# tp = tuple(tuple(l.split('=')) for l in line.split())
+# print(tp)
+
+line = tuple(map(lambda x: tuple(x.split('=')), line))
+print(line)
